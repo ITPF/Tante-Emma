00d3652338fb263f78d62b07467645932e3defbd
Patrick <meine email :)>
2017-06-01 22:12:53 +0200

    frontend

---
+++ src/main/java/org/itpf/tanteemma/frontend/EntryPoint.java
@@ -0,0 +1,30 @@
+package org.itpf.tanteemma.frontend;
+
+import com.vaadin.annotations.Title;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.*;
+
+/**
+ * Created by Patrick on 01.06.2017.
+ */
+@Title("Tante Emma Laden")
+public class EntryPoint extends UI {
+
+
+    protected void init(VaadinRequest vaadinRequest) {
+
+        VerticalLayout content = new VerticalLayout();
+        setContent(content);
+
+        // Display the greeting
+        content.addComponent(new Label("Hello World!"));
+
+        // Have a clickable button
+        content.addComponent(new Button("Push Me!",
+                click -> Notification.show("Pushed!")));
+
+
+    }
+
+
+}
